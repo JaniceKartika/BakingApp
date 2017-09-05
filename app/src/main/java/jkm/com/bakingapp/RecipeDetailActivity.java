c64f448b2882b7c176f9fcce4328fcfb6f130bca
Janice Kartika <janicekartika@gmail.com>
2017-09-05 11:23:41 +0700

    Add support for tablet

--- app/src/main/java/jkm/com/bakingapp/RecipeDetailActivity.java
+++ app/src/main/java/jkm/com/bakingapp/RecipeDetailActivity.java
@@ -1,7 +1,6 @@
 package jkm.com.bakingapp;
 
 import android.content.Intent;
-import android.content.pm.LabeledIntent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -11,6 +10,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -21,16 +21,16 @@ import butterknife.ButterKnife;
 import jkm.com.bakingapp.model.RecipeModel;
 import jkm.com.bakingapp.util.RecipeAssets;
 
-public class RecipeDetailActivity extends AppCompatActivity implements RecipeDetailFragment.OnStepClickListener {
+public class RecipeDetailActivity extends AppCompatActivity implements RecipeDetailFragment.OnStepClickListener,
+        StepDetailFragment.OnPageSelected {
 
-    @BindView(R.id.collapsing_toolbar_recipe_detail)
-    CollapsingToolbarLayout collapsingToolbar;
     @BindView(R.id.iv_recipe_detail)
     ImageView recipeDetailImageView;
     @BindView(R.id.toolbar_recipe_detail)
     Toolbar toolbar;
 
     private RecipeModel recipeModel;
+    private boolean isTwoPane;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,53 +38,94 @@ public class RecipeDetailActivity extends AppCompatActivity implements RecipeDet
         setContentView(R.layout.activity_recipe_detail);
         ButterKnife.bind(this);
 
+        Intent intent = getIntent();
+        if (intent.hasExtra(getString(R.string.recipes_key))) {
+            recipeModel = intent.getParcelableExtra(getString(R.string.recipes_key));
+        } else {
+            Toast.makeText(this, getString(R.string.failed_show_recipe_detail), Toast.LENGTH_LONG).show();
+            return;
+        }
+
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        int orientation = getResources().getConfiguration().orientation;
-        setToolbarViewParams(orientation);
-
-        Intent intent = getIntent();
-        if (intent.hasExtra(getString(R.string.recipes_key))) {
-            recipeModel = intent.getParcelableExtra(getString(R.string.recipes_key));
-
-            collapsingToolbar.setTitle(recipeModel.getName());
-            collapsingToolbar.setCollapsedTitleTextColor(Color.WHITE);
-            collapsingToolbar.setExpandedTitleColor(Color.TRANSPARENT);
-
-            String imagePath = recipeModel.getImage() == null ? "" : recipeModel.getImage();
-            if (imagePath.isEmpty()) {
-                recipeDetailImageView.setImageResource(RecipeAssets.getRecipeImageID(recipeModel.getName()));
-            } else {
-                Picasso.with(this)
-                        .load(imagePath)
-                        .placeholder(R.drawable.placeholder)
-                        .error(R.drawable.placeholder)
-                        .into(recipeDetailImageView);
-            }
+        if (findViewById(R.id.linear_layout_two_pane) != null) {
+            isTwoPane = true;
+            View toolbarShadow = findViewById(R.id.toolbar_shadow_recipe_detail);
+            toolbarShadow.bringToFront();
 
             if (savedInstanceState == null) {
-                RecipeDetailFragment recipeDetailFragment = new RecipeDetailFragment();
-                recipeDetailFragment.setRecipeModel(recipeModel);
+                StepDetailFragment stepDetailFragment = new StepDetailFragment();
+                stepDetailFragment.setStepModels(recipeModel.getSteps());
+                stepDetailFragment.setInitialPosition(0);
+
+                setTitle(recipeModel.getSteps().get(0).getShortDescription());
 
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 fragmentManager.beginTransaction()
-                        .add(R.id.recipe_detail_container, recipeDetailFragment)
+                        .add(R.id.step_detail_container, stepDetailFragment)
                         .commit();
             }
         } else {
-            Toast.makeText(this, getString(R.string.failed_show_recipe_detail), Toast.LENGTH_LONG).show();
+            isTwoPane = false;
+            int orientation = getResources().getConfiguration().orientation;
+            setToolbarViewParams(orientation);
+
+            CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout)
+                    findViewById(R.id.collapsing_toolbar_recipe_detail);
+            collapsingToolbar.setTitle(recipeModel.getName());
+            collapsingToolbar.setCollapsedTitleTextColor(Color.WHITE);
+            collapsingToolbar.setExpandedTitleColor(Color.TRANSPARENT);
+        }
+
+        String imagePath = recipeModel.getImage() == null ? "" : recipeModel.getImage();
+        if (imagePath.isEmpty()) {
+            recipeDetailImageView.setImageResource(RecipeAssets.getRecipeImageID(recipeModel.getName()));
+        } else {
+            Picasso.with(this)
+                    .load(imagePath)
+                    .placeholder(R.drawable.placeholder)
+                    .error(R.drawable.placeholder)
+                    .into(recipeDetailImageView);
+        }
+
+        if (savedInstanceState == null) {
+            RecipeDetailFragment recipeDetailFragment = new RecipeDetailFragment();
+            recipeDetailFragment.setRecipeModel(recipeModel);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .add(R.id.recipe_detail_container, recipeDetailFragment)
+                    .commit();
         }
     }
 
     @Override
+    public void setOnPageSelected(int position) {
+        setTitle(recipeModel.getSteps().get(position).getShortDescription());
+    }
+
+    @Override
     public void onStepSelected(int position) {
-        Intent intent = new Intent(this, StepDetailActivity.class);
-        intent.putParcelableArrayListExtra(getString(R.string.steps_key), recipeModel.getSteps());
-        intent.putExtra(getString(R.string.step_position_key), position);
-        startActivity(intent);
+        if (isTwoPane) {
+            StepDetailFragment stepDetailFragment = new StepDetailFragment();
+            stepDetailFragment.setStepModels(recipeModel.getSteps());
+            stepDetailFragment.setInitialPosition(position);
+
+            setTitle(recipeModel.getSteps().get(position).getShortDescription());
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.step_detail_container, stepDetailFragment)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, StepDetailActivity.class);
+            intent.putParcelableArrayListExtra(getString(R.string.steps_key), recipeModel.getSteps());
+            intent.putExtra(getString(R.string.step_position_key), position);
+            startActivity(intent);
+        }
     }
 
     private void setToolbarViewParams(int orientation) {
@@ -103,5 +144,11 @@ public class RecipeDetailActivity extends AppCompatActivity implements RecipeDet
         CollapsingToolbarLayout.LayoutParams layoutParamsForImageView = new CollapsingToolbarLayout
                 .LayoutParams(CollapsingToolbarLayout.LayoutParams.MATCH_PARENT, imageHeight);
         recipeDetailImageView.setLayoutParams(layoutParamsForImageView);
+    }
+
+    private void setTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
     }
 }
